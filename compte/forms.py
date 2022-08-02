e53451a4faaa22c5b910deec960d7edaa063b6b2
olouyabi <cholouyabi@gmail.com>
2022-08-02 09:01:29 +0100

    Modification du formulaire d'inscription

--- compte/forms.py
+++ compte/forms.py
@@ -8,8 +8,57 @@ from compte.models import MembreUser
 
 
 class RegistrationForm(forms.ModelForm):
+    username = forms.CharField(max_length=200, label="Nom d'utilisateur", required=True, widget=forms.TextInput(attrs={
+        'class':"form-control", 
+        'id':"username", 
+        'type':"text", 
+        'placeholder':"Nom d'utilisateur", 
+        'data-sb-validations':"required",
+        'name':"username"
+    }))
+    first_name = forms.CharField(max_length=200, label="Prénom", required=True, widget=forms.TextInput(attrs={
+        'class':"form-control", 
+        'id':"prenom", 
+        'type':"text", 
+        'placeholder':"Prénom", 
+        'data-sb-validations':"required",
+        'name':"prenom"
+    }))
+    last_name = forms.CharField(max_length=200, label="Nom", required=True, widget=forms.TextInput(attrs={
+        'class':"form-control", 
+        'id':"nom", 
+        'type':"text", 
+        'placeholder':"Nom", 
+        'data-sb-validations':"required",
+        'name':"nom"
+    }))
+    email = forms.EmailField(max_length=200, label="Adresse mail", required=True, widget=forms.TextInput(attrs={
+        'class':"form-control", 
+        'id':"email", 
+        'type':"email", 
+        'placeholder':"Adresse mail", 
+        'data-sb-validations':"required",
+        'name':"email"
+    }))
+    password = forms.CharField(max_length=200, label="Mot de passe", required=True, widget=forms.TextInput(attrs={
+        'class':"form-control", 
+        'id':"password", 
+        'type':"password", 
+        'placeholder':"Mot de passe", 
+        'data-sb-validations':"required",
+        'name':"password"
+    }))
+    shipping_id = forms.CharField(max_length=20, label="Code de livraison", required=True, widget=forms.TextInput(attrs={
+        'class':"form-control", 
+        'id':"shippingid", 
+        'type':"text", 
+        'placeholder':"Code de livraison", 
+        'data-sb-validations':"required",
+        'name':"shippingid"
+    }))
+
 
     class Meta:
         model = MembreUser
-        fields = '__all__'
+        fields = ('username', 'first_name','last_name','password', 'shipping_id',)
 
--- compte/models.py
+++ compte/models.py
@@ -7,6 +7,7 @@ class MembreUser(models.Model):
     username = models.CharField(max_length=200, null=True, blank=False, verbose_name="Nom d'utilisateur")
     first_name = models.CharField(max_length=200, null=True, blank=True, verbose_name="Prénom")
     last_name = models.CharField(max_length=200, null=True, blank=True, verbose_name="Prénom")
+    email = models.EmailField(max_length=200, null=True, blank=True, verbose_name="Adresse mail")
     password = models.CharField(max_length=20, null=True, blank=False, verbose_name="Mot de passe")
     shipping_id = models.CharField(max_length=20, null=True, blank=False, verbose_name="Code de livraison")
     bio = models.TextField(default="Aucun bio n'est disponible...")
