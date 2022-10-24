9446e2b099bb768f1248069e747224e7fc367e9d
olouyabi <cholouyabi@gmail.com>
2022-10-24 08:31:40 +0100

    config-for-deploy

--- comptamatnex/settings.py
+++ comptamatnex/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-frm!*2zys!cwy-d-k-cv1nmu=-e9)!c6ce5bw2gyud(ald@wbi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
